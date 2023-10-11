1dce51d89bc60966e8971d262d0091245edee48c
DucaAdina <115735581+DucaAdina@users.noreply.github.com>
2023-10-11 14:01:52 +0300

    first commit

--- first.cpp
+++ first.cpp
@@ -1,19 +1,6 @@
-#include <string>
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-struct monster
-{
-    int health;
-    int attack;
-    int defense;
-    string name;
-};
-
-int main()
-{
-monster dragon={100,50,5,"Mimi"};
-cout  << " monster name=" << dragon.name << endl;
-return 0;
+int main(){
+    cout << "Hello world" ;
+    return 0;
 }
